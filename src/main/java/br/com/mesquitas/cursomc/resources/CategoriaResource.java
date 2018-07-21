bc443479ab696e892e638fcb7f0e8b619fffda7e
unknown <adxmesquita@gmail.com>
2018-07-21 14:19:28 -0300

    REST testado com êxito

---
+++ src/main/java/br/com/mesquitas/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package br.com.mesquitas.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Alessandro Mesquita
+ *
+ */
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String listar() {
+		return "REST está funcionando!";
+	}
+}
