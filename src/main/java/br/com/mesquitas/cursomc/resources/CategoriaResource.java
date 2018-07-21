5efc93df113fb0ae3bab42bb12232f4a63c41e15
unknown <adxmesquita@gmail.com>
2018-07-21 14:31:21 -0300

    Testando a primeira classe de domínio

--- src/main/java/br/com/mesquitas/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/mesquitas/cursomc/resources/CategoriaResource.java
@@ -3,9 +3,14 @@
  */
 package br.com.mesquitas.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.mesquitas.cursomc.domain.Categoria;
 
 /**
  * @author Alessandro Mesquita
@@ -17,7 +22,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
